a78dad460583303d43db9721de578a88fc5c9444
icebolt2357 <40649416+icebolt2357@users.noreply.github.com>
2020-02-13 01:22:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
-const formidable = require('formidable');
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
