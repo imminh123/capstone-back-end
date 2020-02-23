e7ea0f812a829941e76b03ad4f1e6304edb289a1
icebolt2357 <40649416+icebolt2357@users.noreply.github.com>
2020-02-23 20:05:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -62,12 +62,12 @@ mongoose.connection.once('open', function () {
     });
 
 //solution for cors error
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 /**
  * Express configuration.
