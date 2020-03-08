cb1068d16fe3aa341a2facd2d2603e4dcfe9ba42
icebolt2357 <40649416+icebolt2357@users.noreply.github.com>
2020-03-09 00:15:43 +0700

    update note ispinned

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -34,7 +34,7 @@ exports.updateNoteByID = async (req,res) => {
     var index=req.body.index;
     var isPinned=req.body.inPinned;
     //check if all fields are filled
-    if (isEmpty(folderID)||isEmpty(note)||isEmpty(description)||isEmpty(url)||isEmpty(index))
+    if (isEmpty(folderID)||isEmpty(note)||isEmpty(description)||isEmpty(url)||isEmpty(index)||isEmpty(isPinned))
                 res.send(msgEmpty()); 
         else {
             res.send(await NoteDAO.updateNote(noteID,folderID,note,description,url,index,isPinned));                    
