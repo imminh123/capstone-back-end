a532ce0902c4bb29f91d6d43fd24734e8d6c3f2f
icebolt2357 <40649416+icebolt2357@users.noreply.github.com>
2020-03-09 13:04:38 +0700

    x

--- dao/NoteDAO.js
+++ dao/NoteDAO.js
@@ -90,7 +90,7 @@ exports.updateNote = async function(noteID,folderID,note,description,url,index,i
         +'"dateModified": "'+noteentity.dateModified+'"'
     +'}}';
     // console.log(result);
-    return (result);
+    return 'hello';
     // return makeJson('Success','Update successfully');
 }
 
