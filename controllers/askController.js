e3dca434ddd8c9e12b2bcff06bcb9b299c3ef0e6
icebolt2357 <40649416+icebolt2357@users.noreply.github.com>
2020-03-25 23:47:01 +0700

    Update askController.js

--- controllers/askController.js
+++ controllers/askController.js
@@ -17,7 +17,7 @@ exports.createAsk = async (req,res) => {
     var teacher=req.body.teacher;
     var courseURL=req.body.courseURL;
     //check if all fields are filled
-    if (isEmpty(scannedContent)||isEmpty(askContent)||isEmpty(student)||isEmpty(teacher)||isEmpty(courseURL))
+    if (isEmpty(scannedContent)||isEmpty(askContent)||isEmpty(student)||isEmpty(teacher))
                 res.send(msgEmpty()); 
         else {
             res.send(await AskDAO.createAsk(scannedContent,askContent,student,teacher,courseURL));                    
