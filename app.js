76d903af5db3c89071a23e093d6e5b6ec7295257
Minh Nguyen <minhco12@gmail.comm>
2020-04-02 23:30:29 +0700

    change url

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ app.post('/login', (req, res, next) => {
  */
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'], accessType: 'offline', prompt: 'consent' }, { display: 'popup' }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'http://noteitfu.herokuapp.com' }), (req, res) => {
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: 'http://localhost:3002' }), (req, res) => {
   // res.redirect(req.session.returnTo || '/');
   const user = res.req.user;
   jwt.sign({user: user}, 'tinhanhem', (err, token) => { 
@@ -218,7 +218,7 @@ app.get('/auth/google/callback', passport.authenticate('google', { failureRedire
     
     console.log(token)
     // res.cookie('user', token , { domain: 'http://192.168.1.205:3000', maxAge: 900000})
-    res.redirect('http://noteitfu.herokuapp.com?token=' + token);
+    res.redirect('http://localhost:3002?token=' + token);
 
   })
 
